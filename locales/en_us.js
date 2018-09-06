aff4ca883be6a6c272402c7353235fb8361ab53d
yiz0816 <zhangyicoco@hotmail.com>
2018-09-06 23:56:03 +0800

    First Commit

---
+++ locales/en_us.js
@@ -0,0 +1,276 @@
+/**
+ * language: english
+ */
+const lang = {
+
+  //app desc
+  app: {
+    desc: 'electerm is a terminal/ssh/sftp client(linux, mac, win) based on electron/node-pty/xterm/antd and many other libs',
+    isRunning: 'is running',
+    press: 'press',
+    toShow: 'to show'
+  },
+
+  //app menu
+  menu: {
+    //mac app
+    hide: 'hide',
+    hideothers: 'hide others',
+    unhide: 'unhide',
+    quit: 'quit',
+
+    //edit
+    edit: 'edit',
+    undo: 'undo',
+    redo: 'redo',
+    cut: 'cut',
+    copy: 'copy',
+    paste: 'paste',
+    pasteandmatchstyle: 'paste and match style',
+    del: 'delete',
+    selectall: 'select all',
+    startspeaking: 'start speaking',
+    stopspeaking: 'stop speaking',
+
+    //view
+    view: 'view',
+    reload: 'reload',
+    forcereload: 'force reload',
+    toggledevtools: 'toggle dev tools',
+    toggleControl: 'toggle control buttons',
+    resetzoom: 'reset zoom',
+    zoomin: 'zoom in',
+    zoomout: 'zoom out',
+    togglefullscreen: 'toggle fullscreen',
+
+    //window
+    window: 'window',
+    minimize: 'minimize',
+    close: 'close',
+    restart: 'restart',
+    front: 'front',
+
+    //help
+    help: 'help',
+    about: 'about',
+    checkUpdate: 'check update',
+    reportIssue: 'report issue',
+    homepage: 'homepage'
+  },
+
+  //common
+  common: {
+    history: 'history',
+    bookmarks: 'Connection',
+    bookmarkCategory: 'Folder',
+    setting: 'setting',
+    about: 'about',
+    ok: 'ok',
+    cancel: 'cancel'
+  },
+
+  //control buttons
+  control: {
+    author: 'author',
+    download: 'download',
+    bugReport: 'bug report',
+    checkForUpdate: 'check for update',
+    homepage: 'homepage',
+    notFoundContent: 'no item',
+    newSsh: 'new ssh',
+    newTerminal: 'new terminal',
+    dependencies: 'dependencies',
+    env: 'env',
+    os: 'os',
+    userTips: 'user tips'
+  },
+
+  //transferHistory
+  transferHistory: {
+    transferHistory: 'transfer history',
+    localPath: 'local path',
+    remotePath: 'remote path',
+    type: 'type',
+    startTime: 'start time',
+    finishTime: 'finish time',
+    speed: 'speed',
+    clear: 'clear'
+  },
+
+  //ssh tabs
+  tabs: {
+    titleEmptyWarn: 'title can not be empty',
+    close: 'close',
+    closeOtherTabs: 'close other tabs',
+    closeTabRight: 'close tabs on the right',
+    newTab: 'new tab',
+    duplicate: 'duplicate',
+    rename: 'rename',
+    openNewTerm: 'open new terminal'
+  },
+
+  //main warpper
+  main: {
+    error: 'Something went wrong.'
+  },
+
+  //updater check
+  updater: {
+    noNeed: 'no need to update',
+    noNeedDesc: 'you are using the latest release',
+    fail: 'check update fails',
+    goGetIt: 'go get it!',
+    newVersion: 'new version found'
+  },
+
+  //setting
+  setting: {
+    new: 'new',
+    settings: 'settings',
+    common: 'common',
+    hotkeyNotOk: 'hotkey can not be registe, please use another one',
+    saved: 'saved',
+    saveLang: 'saved, restart to take effect',
+    restartNow: 'restart now',
+    hotkeyDesc: 'system hotkey(bring window back to front)',
+    timeoutDesc: 'ssh timeout(in millisecond)',
+    scrollBackDesc: 'scrollback lines',
+    language: 'language'
+  },
+
+  //sftp
+  sftp: {
+    cancel: 'cancel',
+    skip: 'skip',
+    merge: 'merge',
+    overwrite: 'overwrite',
+    rename: 'rename',
+    renameAll: 'rename all',
+    mergeDesc: 'merge rest conflict folders',
+    overwriteDesc: 'overwrite rest conflict files',
+    mergeAll: 'merge all',
+    overwriteAll: 'overwrite all',
+    renameDesc: 'rename rest files/folders',
+    folder: 'folder',
+    file: 'file',
+    fileConflict: 'file conflict',
+    submit: 'submit',
+    edit: 'edit',
+    open: 'open',
+    permission: 'permission',
+    name: 'name',
+    mode: 'mode',
+    path: 'path',
+    fullPath: 'full path',
+    size: 'size',
+    accessTime: 'access time',
+    modifyTime: 'modify time',
+    attributes: 'attributes',
+    enter: 'enter',
+    deleteAll: 'delete all',
+    selected: 'selected',
+    newFile: 'new file',
+    newFolder: 'new folder',
+    selectAll: 'select all',
+    refresh: 'refresh',
+    editPermission: 'edit permission',
+    info: 'info',
+    filesAndFolders: 'files/folders',
+    files: 'files',
+    delTip: 'are you sure? this will delete these',
+    delTip1: 'and all the file/directory in them',
+    goParent: 'goto parent folder',
+    hide: 'hide',
+    show: 'show',
+    hfd: 'hidden files and directories',
+    remote: 'remote',
+    local: 'local',
+    fileTransfers: 'file transfers',
+    cancelAll: 'cancel all',
+    upload: 'upload',
+    download: 'download',
+    resume: 'resume',
+    pause: 'pause'
+  },
+
+  permission: {
+    read: 'read',
+    write: 'write',
+    exec: 'exec',
+    owner: 'owner',
+    group: 'group',
+    other: 'other'
+  },
+
+  //ssh form
+  form: {
+    password: 'password',
+    privateKey: 'privateKey',
+    privateKeyDesc: 'private key string',
+    importFromFile: 'import from file',
+    passphrase: 'passphrase',
+    passphraseDesc: 'passphrase for privateKey',
+    host: 'host',
+    username: 'username',
+    port: 'port',
+    title: 'title',
+    saveAndConnect: 'save and connect',
+    save: 'save',
+    loginScript: 'run script',
+    loginScriptTip: 'run script after logined',
+    connect: 'connect',
+    testConnection: 'test connection',
+    required: 'required',
+    keyId:'keyId'
+  },
+
+  //ssh terminal
+  ssh: {
+    clear: 'clear',
+    selectAll: 'select all',
+    savePassword: 'save password',
+    search: 'search',
+    terminal: 'terminal',
+    nextMatch: 'next match',
+    prevMatch: 'prev match',
+    split: 'split',
+    fileManager: 'file manager',
+    changeDirection: 'change direction'
+  },
+
+  //user tips
+  userTips: [
+    '<b>double click</b> a ssh tab will duplicate a same tab, and enter same path as original tab(limited support)',
+    '<b>double click</b> blank area of tab bar will open a new ssh terminal too',
+    '<b>right click</b> a ssh tab you can open context menu, from context menu there are <b>close other tabs</b>, <b>close tabs on the right</b>, and other functions',
+    'by default, <b>Ctrl + 2</b> will bring electerm window to front, hotkey can be set in <b>setting</b> menu',
+    'sftp copy and paste works in both local and remote'
+  ],
+
+  //features
+  features: [
+    'work as a terminal or ssh/sftp client(similar to xshell)',
+    'hotkey to bring window back to front(simliar to guake, default is ctrl + 2)',
+    'support multi platform(linux, mac, windows)',
+    'support multi-language(en, cn only for now)'
+  ],
+
+  featuresName: 'features',
+
+  //terminal themes
+  terminalThemes: {
+    terminalThemes: 'terminal themes',
+    export: 'export',
+    themeName: 'theme name',
+    saveAndApply: 'save and apply',
+    apply: 'apply',
+    default: 'default',
+    newTheme: 'new theme',
+    themeConfig: 'theme config'
+  }
+}
+
+module.exports = {
+  lang,
+  name: 'English'
+}
---
+++ locales/zh_cn.js
@@ -0,0 +1,277 @@
+/**
+ * language: 中文
+ */
+const lang = {
+
+  //app desc
+  app: {
+    desc: 'electerm是一个terminal/ssh/sftp客户端(支持linux, mac, win), 基于electron/node-pty/xterm/antd等组件',
+    isRunning: '运行中',
+    press: '按',
+    toShow: '显示'
+  },
+
+  //app menu
+  menu: {
+    //mac app
+    hide: '隐藏',
+    hideothers: '隐藏其他',
+    unhide: '显示',
+    quit: '退出',
+
+    //edit
+    edit: '编辑',
+    undo: '撤销',
+    redo: '重做',
+    cut: '剪切',
+    copy: '复制',
+    paste: '粘贴',
+    pasteandmatchstyle: '带格式粘贴',
+    del: '删除',
+    selectall: '全选',
+    startspeaking: '开始录音',
+    stopspeaking: '停止录音',
+
+    //view
+    view: '视图',
+    reload: '刷新',
+    forcereload: '强制刷新',
+    toggledevtools: '切换开发者工具',
+    toggleControl: '切换显示/隐藏控制按钮',
+    resetzoom: '重设大小',
+    zoomin: '缩小',
+    zoomout: '放大',
+    togglefullscreen: '切换全屏',
+
+    //window
+    window: '窗口',
+    minimize: '最小化',
+    close: '关闭',
+    restart: '重启',
+    front: '到最前',
+
+    //help
+    help: '帮助',
+    about: '关于',
+    checkUpdate: '检查更新',
+    reportIssue: '报告问题',
+    homepage: '官网'
+  },
+
+  //common
+  common: {
+    history: '访问历史',
+    bookmarks: '书签',
+    bookmarkCategory: '书签分类',
+    setting: '设置',
+    about: '关于',
+    ok: '好的',
+    cancel: '取消'
+  },
+
+  //control buttons
+  control: {
+    author: '作者',
+    download: '下载',
+    bugReport: '提交bug',
+    checkForUpdate: '检查更新',
+    homepage: '官网',
+    notFoundContent: '还没有内容',
+    newSsh: '新建ssh',
+    newTerminal: '新连接',
+    dependencies: '依赖组件',
+    env: '环境变量',
+    os: '操作系统',
+    userTips: '使用小技巧'
+  },
+
+  //transferHistory
+  transferHistory: {
+    transferHistory: '传输历史记录',
+    localPath: '本地路径',
+    remotePath: '远程路径',
+    type: '类型',
+    startTime: '开始时间',
+    finishTime: '结束时间',
+    speed: '速度',
+    clear: '清空'
+  },
+
+  //ssh tabs
+  tabs: {
+    titleEmptyWarn: '标题不能为空',
+    close: '关闭',
+    closeOtherTabs: '关闭其他连接',
+    closeTabRight: '关闭右侧连接',
+    newTab: '新建连接',
+    duplicate: '复制',
+    rename: '重命名',
+    openNewTerm: '打开新连接'
+  },
+
+  //main warpper
+  main: {
+    error: '出错了.'
+  },
+
+  //updater check
+  updater: {
+    noNeed: '无需升级',
+    noNeedDesc: '你已经在使用最新版本',
+    fail: '检查升级失败了',
+    goGetIt: '前往升级!',
+    newVersion: '发现新版本'
+  },
+
+  //setting
+  setting: {
+    new: '新建',
+    settings: '设置',
+    common: '通用',
+    hotkeyNotOk: '快捷键无法使用，请换一个试试',
+    saved: '已保存',
+    saveLang: '已保存, 重启生效',
+    restartNow: '立刻重启',
+    hotkeyDesc: '系统快捷键(重新把窗口带到最前面)',
+    timeoutDesc: 'ssh超时时间(毫秒)',
+    scrollBackDesc: '保留的行数',
+    language: '语言'
+  },
+
+  //sftp
+  sftp: {
+    cancel: '取消',
+    skip: '跳过',
+    merge: '合并',
+    overwrite: '覆盖',
+    rename: '重命名',
+    renameAll: '重命名所有',
+    mergeDesc: '合并所有冲突',
+    overwriteDesc: '覆盖所有冲突文件',
+    mergeAll: '合并所有',
+    overwriteAll: '覆盖所有',
+    renameDesc: '重命名余下的文件/文件夹',
+    folder: '文件夹',
+    file: '文件',
+    fileConflict: '文件冲突',
+    submit: '提交',
+    edit: '编辑',
+    open: '打开',
+    permission: '权限',
+    name: '名称',
+    mode: '模式',
+    path: '路径',
+    fullPath: '完整路径',
+    size: '大小',
+    accessTime: '访问时间',
+    modifyTime: '修改时间',
+    attributes: '属性',
+    enter: '进入',
+    deleteAll: '删除所有',
+    selected: '选中的',
+    newFile: '新建文件',
+    newFolder: '新建文件夹',
+    selectAll: '全选',
+    refresh: '刷新',
+    editPermission: '编辑权限',
+    info: '文件信息',
+    filesAndFolders: '文件/文件夹',
+    files: '文件',
+    delTip: '确定么？将会删除这些',
+    delTip1: '以及其中的文件/文件夹',
+    goParent: '前往上级文件夹',
+    hide: '隐藏',
+    show: '显示',
+    hfd: '隐藏文件和文件夹',
+    remote: '远程',
+    local: '本地',
+    fileTransfers: '文件传输',
+    cancelAll: '取消所有',
+    upload: '上传',
+    download: '下载',
+    resume: '继续',
+    pause: '暂停'
+  },
+
+  permission: {
+    read: '读',
+    write: '写',
+    exec: '执行',
+    owner: '所有者',
+    group: '组',
+    other: '其他'
+  },
+
+  //ssh form
+  form: {
+    password: '密码',
+    privateKey: '私匙',
+    privateKeyDesc: '私匙字符串',
+    importFromFile: '从文件导入',
+    passphrase: '私匙密码',
+    passphraseDesc: '私匙的密码',
+    host: '主机地址',
+    username: '用户名',
+    port: '端口',
+    title: '标题',
+    saveAndConnect: '保存并连接',
+    save: '保存',
+    loginScript: '运行脚本',
+    loginScriptTip: '登陆后运行脚本',
+    connect: '连接',
+    testConnection: '测试连接',
+    required: '必填',
+    keyId:'keyId'
+
+  },
+
+  //ssh terminal
+  ssh: {
+    clear: '清屏',
+    selectAll: '全选',
+    savePassword: '保存密码',
+    search: '搜索',
+    terminal: '终端',
+    nextMatch: '下一处',
+    prevMatch: '上一处',
+    split: '分割',
+    fileManager: '文件管理器',
+    changeDirection: '切换布局'
+  },
+
+  //user tips
+  userTips: [
+    '<b>双击</b>一个标签即可复制一个新标签，并且自动进入相同的路径(有限支持)',
+    '<b>双击</b>标签栏空白处也可以打开一个新标签',
+    '<b>右键单击</b>标签页打开菜单, 有<b>关闭其他标签</b>, <b>关闭右侧连接</b>等功能',
+    '默认情况下, 按下组合键<b>Ctrl + 2</b>就会把窗口重新激活置于最前, 热键可以在设置页面设置',
+    'sftp的复制粘贴功能可以任意使用在远程和本地文件管理器'
+  ],
+
+  //features
+  features: [
+    'terminal/ssh/sftp客户端(类似xshell)',
+    '支持热键唤醒',
+    '多平台支持(linux, mac, windows)',
+    '多语言支持'
+  ],
+
+  featuresName: '特性',
+
+  //terminal themes
+  terminalThemes: {
+    terminalThemes: '终端主题',
+    export: '导出',
+    themeName: '主题名称',
+    saveAndApply: '保存并应用',
+    apply: '应用',
+    default: '默认',
+    newTheme: '新主题',
+    themeConfig: '主题定义'
+  }
+}
+
+module.exports = {
+  lang,
+  name: '中文'
+}
